cd95566e9add2874dcfb79a4abf79501e6745345
shifaz24 <52294542+shifaz24@users.noreply.github.com>
2021-04-02 20:57:53 +0530

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,13 +1,13 @@
-gap<- read.csv("household_power_consumption.txt", header=T, sep=';', na.strings="?", 
+gap<- read.csv("householdpowerconsumption.txt", header=T, sep=';', na.strings="?", 
                nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
 gap$Date <- as.Date(gap$Date, format="%d/%m/%Y")
 
 
-subset <- subset(gap, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+set <- subset(gap, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
 
 
 
-hist(subset$Global_active_power, main="Global Active Power", 
+hist(set$Global_active_power, main="Global Active Power", 
      xlab="Global Active Power ", ylab="Frequency", col="red")
 
 dev.copy(png, file="plot1.png", height=480, width=480)
