e5a4a177fa746b9395593d5a89e32ec4eed74479
shifaz24 <52294542+shifaz24@users.noreply.github.com>
2021-04-02 21:12:27 +0530

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,11 +1,11 @@
-gap <- read.csv("household_power_consumption.txt", header=T, sep=';', na.strings="?", nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
+gap <- read.csv("householdpowerconsumption.txt", header=T, sep=';', na.strings="?", nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
 gap$Date <- as.Date(gap$Date, format="%d/%m/%Y")
 
-subset <- subset(gap, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+set <- subset(gap, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
 
 
-conv_subset <- paste(as.Date(subset$Date), subset$Time)
-subset$Datetime <- as.POSIXct(conv_subset)
+conv_subset <- paste(as.Date(set$Date),set$Time)
+set$Datetime <- as.POSIXct(conv_subset)
 
 plot(subset$Global_active_power,  type="l", ylab="Global Active Power (kilowatts)", xlab="")
 
